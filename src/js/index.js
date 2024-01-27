ddc098a7984c4dfd1afc73f0df76db6eaf20964b
RafaelContact <contactrodrigues21@gmail.com>
2024-01-27 02:02:48 -0300

    feat: global objects and warning

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,35 @@ const inputs = {
     name : document.querySelector('.name'),
     tel : document.querySelector('.tel'),
     checkbox : document.querySelector('#confirm'),
-    sub_btn : document.querySelector('.submit')
+    sub_btn : document.querySelector('.submit'),
+    verify : document.querySelectorAll('.check'),
+    warning : document.querySelector('.warning')
 }
 
 inputs.sub_btn.addEventListener('click', (e)=> {
     e.preventDefault()
+    let i = 0
+    let i2 = 0
+    inputs.verify.forEach(element => {
+        if(inputs.checkbox.checked == false || element.value !== ""){
+            i2++
+            if(i2 == 1){ warning() }
+            
+        } else if(inputs.checkbox.checked && element.value !== ""){
+            i++
+            let test = element.value
+            test = test.split(" ")
+            // console.log(test.length)
+            if(i === 2){
+                console.log(i)
+            }
+        }
+    });
+    
 })
+let warning = ()=>{
+    let warning = document.createElement("p")
+    let content = document.createTextNode("Preencha todos os campos antes de enviar")
+        warning.appendChild(content)
+        inputs.warning.appendChild(warning)
+}
